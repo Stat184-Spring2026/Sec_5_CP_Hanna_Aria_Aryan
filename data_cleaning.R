6e9b43ed1c51246665090a4e99356cc891544aaa
hannaoh526 <hannaoh.526@gmail.com>
2026-04-27 01:27:02 -0400

    Update data_cleaning.R to include only cleaning steps

--- data_cleaning.R
+++ data_cleaning.R
@@ -36,13 +36,3 @@ nrow(food_clean)
 
 food_clean |>
   count(item_clean)
-
-ggplot(food_clean, aes(x = date, y = value, color = item_clean)) +
-  geom_line(linewidth = 1) +
-  labs(
-    title = "Average Prices of Selected Food Items Over Time",
-    x = "Year",
-    y = "Average Price (USD)",
-    color = "Food Item"
-  ) +
-  theme_minimal()
